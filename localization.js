21b678b8e338d283b245c1e638b455f058ef79a1
riccardone <riccardo@dinuzzo.it>
2014-12-11 10:31:51 +0000

    Normalize language code
    Added a private function to read two parts language codes (ex it-IT )

--- localization.js
+++ localization.js
@@ -1,49 +1,59 @@
 var Localization;
 
-Localization = (function() {
+Localization = (function () {
 
-  function Localization() {}
+    function Localization() { }
 
-  Localization.initialize = function(dictionnary, fallback_language) {
-    Localization.dictionnary = dictionnary;
-    Localization.fallback_language = fallback_language;
-    return navigator.globalization.getPreferredLanguage(Localization.get_preferred_language_callback, Localization.get_preferred_language_error_callback);
-  };
+    Localization.initialize = function (dictionnary, fallback_language) {
+        Localization.dictionnary = dictionnary;
+        Localization.fallback_language = fallback_language;
+        return navigator.globalization.getPreferredLanguage(Localization.get_preferred_language_callback, Localization.get_preferred_language_error_callback);
+    };
 
-  Localization.get_preferred_language_callback = function(language) {
-    Localization.language = language.value;
-    console.log("Phone language is " + Localization.language);
-    if (Localization.language in Localization.dictionnary) {
-      console.log("It is supported.");
-    } else {
-      Localization.language = Localization.fallback_language;
-      console.log("It is unsupported, so we chose " + Localization.language + " instead.");
+    function normalizeLanguage(language) {
+        if (language.indexOf('-') !== -1) {
+            return language.split('-')[0];
+        }
+        if (language.indexOf('_') !== -1) {
+            return language.split('_')[0];
+        }
+        return language;
     }
-    return Localization.apply_to_current_html();
-  };
 
-  Localization.get_preferred_language_error_callback = function() {
-    Localization.language = Localization.fallback_language;
-    console.log("There was a error determining the language, so we chose " + Localization.language + ".");
-    return Localization.apply_to_current_html();
-  };
+    Localization.get_preferred_language_callback = function (language) {
+        Localization.language = normalizeLanguage(language.value);
+        console.log("Phone language is " + Localization.language);
+        if (Localization.language in Localization.dictionnary) {
+            console.log("It is supported.");
+        } else {
+            Localization.language = Localization.fallback_language;
+            console.log("It is unsupported, so we chose " + Localization.language + " instead.");
+        }
+        return Localization.apply_to_current_html();
+    };
 
-  Localization.apply_to_current_html = function() {
-    var key, value, _ref, _results;
-    console.log("Localizing HTML file.");
-    _ref = Localization.dictionnary[Localization.language];
-    _results = [];
-    for (key in _ref) {
-      value = _ref[key];
-      _results.push($(".l10n-" + key).html(value));
-    }
-    return _results;
-  };
+    Localization.get_preferred_language_error_callback = function () {
+        Localization.language = Localization.fallback_language;
+        console.log("There was a error determining the language, so we chose " + Localization.language + ".");
+        return Localization.apply_to_current_html();
+    };
 
-  Localization["for"] = function(key) {
-    return Localization.dictionnary[Localization.language][key];
-  };
+    Localization.apply_to_current_html = function () {
+        var key, value, _ref, _results;
+        console.log("Localizing HTML file.");
+        _ref = Localization.dictionnary[Localization.language];
+        _results = [];
+        for (key in _ref) {
+            value = _ref[key];
+            _results.push($(".l10n-" + key).html(value));
+        }
+        return _results;
+    };
 
-  return Localization;
+    Localization["for"] = function (key) {
+        return Localization.dictionnary[Localization.language][key];
+    };
+
+    return Localization;
 
 })();
